990e085e4b3e074d1e40c932c1813f86203eb661
dolf <to.dolfin@gmail.com>
2022-09-03 18:55:18 +0300

    background scope

--- app/src/main/java/ru/tgfd/android/MainActivity.kt
+++ app/src/main/java/ru/tgfd/android/MainActivity.kt
@@ -5,7 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import ru.tgfd.android.telegram.TelegramApi
 import ru.tgfd.ui.state.UiState
 import ru.tgfd.core.Calendar
@@ -17,15 +18,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
         val authorizationApi = TelegramApi(this)
         val calendar = object : Calendar {
             override fun now() = java.util.Calendar.getInstance().time.time
         }
-        val repository = FeedFacadeImpl(FeedRepositoryStub(), lifecycleScope)
+        val repository = FeedFacadeImpl(FeedRepositoryStub(), backgroundScope)
 
         val uiState = UiState.Builder
             .api(authorizationApi)
-            .scope(lifecycleScope)
+            .scope(backgroundScope)
             .calendar(calendar)
             .repository(repository)
             .build()
