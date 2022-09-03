fb00af5ff72e0965f7da5ec5168800a16d3798bc
dolf <to.dolfin@gmail.com>
2022-09-03 20:27:57 +0300

    date format

--- app/src/main/java/ru/tgfd/android/Extensions.kt
+++ app/src/main/java/ru/tgfd/android/Extensions.kt
@@ -1,20 +1,11 @@
 package ru.tgfd.android
 
+import java.text.DateFormat
 import java.util.*
 
-fun Int.toTwoDigits() = if (this < 10) "0$this" else this.toString()
-
 fun Long.toStringData(): String {
-    val timeMillis = this
-    val publicationCalendar = Calendar.getInstance().apply {
-        timeInMillis = timeMillis
-    }
-    val dayOfMonth = publicationCalendar.get(Calendar.DAY_OF_MONTH).toTwoDigits()
-    val monthNumber = (publicationCalendar.get(Calendar.MONTH) + 1).toTwoDigits()
-    val hours = publicationCalendar.get(Calendar.HOUR).toTwoDigits()
-    val minutes = publicationCalendar.get(Calendar.MINUTE).toTwoDigits()
-    val year = publicationCalendar.get(Calendar.YEAR)
-    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
-    val yearString = if (year != currentYear) ".$year" else ""
-    return "$dayOfMonth.$monthNumber$yearString в $hours:$minutes"
+    val calendar = Calendar.getInstance()
+    calendar.timeInMillis = this * 1000
+    val formatter = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT)
+    return formatter.format(calendar.time)
 }
